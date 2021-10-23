45eb983570f0d32764fd0068e8ce4690897e5303
Chris Noble <noblechrisj@gmail.com>
2021-10-23 16:41:55 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learning hot tu use assembly on a Raspberry Pi
---
+++ first.s
@@ -0,0 +1,6 @@
+.global main
+
+main:
+	mov r0, #2 /* put a 2 in the r0 register*/
+	bx lr /* return from main, branch to left register????*/
+	
